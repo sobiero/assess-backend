29fa5030494ac69eee326ec88d53f77d80623195
Simon Mogaka <simon.mogaka@un.org>
2020-03-16 08:24:31 +0100

    WiP

--- app/config/loader.php
+++ app/config/loader.php
@@ -5,9 +5,9 @@ $loader = new \Phalcon\Loader();
 $loader->registerNamespaces(
   [
     'Project\Evaluation'  => APP_PATH . '/modules/evaluation/classes/',
-	'Dmkit\Phalcon'       => APP_PATH . '/libraries/dmkit/Phalcon/', //for PHP 7
-	'EmoG\Phalcon'        => APP_PATH . '/libraries/emog/Phalcon/', //for PHP 5.6
-	'Firebase\JWT'        => APP_PATH . '/libraries/firebase/', //for PHP 5.6
+	'Dmkit\Phalcon'       => APP_PATH . '/libraries/jwt/dmkit/Phalcon/', //for PHP 7
+	'EmoG\Phalcon'        => APP_PATH . '/libraries/jwt/emog/Phalcon/', //for PHP 5.6
+	'Firebase\JWT'        => APP_PATH . '/libraries/jwt/firebase/', //for PHP 5.6
 
 
   ]
